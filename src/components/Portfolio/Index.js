e8e022f807d5e85539f1762b1be27cd2c3badf20
viadel13 <shababizetre@gmail.com>
2023-08-11 22:30:25 +0100

    first commit

--- src/components/Portfolio/Index.js
+++ src/components/Portfolio/Index.js
@@ -6,17 +6,32 @@ import { Link } from 'react-router-dom';
 
 const Portfolio = () => {
   return (
-    <div id="contenu2">
-      
+    <div id="contenu1">
+
     <article className="gauche">
-      <h2>Notre portfolio</h2>
-        <Link to="#"><img src={transport1} alt='transport1' /></Link>
-        <Link to="#"><img src={transport2} alt='transport2' /></Link>
-        <Link to="#"><img src={transport3} alt='transport3' /></Link>
-        <Link to="#"><img src={transport4} alt='transport4' /></Link>
+      <h2>Qui Sommes nous?</h2>
+      <h3>TTNRC Sarl :Transit Transport negoce Representation Commerciale</h3>
+      <p> Pr&eacute;sentation du portfolio et de l'expérience de l'entreprise <strong>TTNRC</strong> dans ses déploiements au Littoral, au Centre, au Nord et dans la sous région Afrique Centrale.
+         </p>
+      <h3>Nos compétences</h3>
+      <h4>En logistique import et export, par mer-air-terre</h4>
       
+      <ul>
+        <li>Proc&eacute;dure de pr&eacute;d&eacute;douanement des marchandises</li>
+        <li>Proc&eacute;dure de d&eacute;douanement</li>
+        <li>Proc&eacute;dure d'enl&egrave;vement selon les cas ci-apr&egrave;s:</li>            
+      </ul>
+      
+      <ul>
+        <li>Mise &agrave; la consommation directe/suite &agrave; un r&eacute;gime suspensif</li>
+          <li>Admission temporaire normale-Admission temporaire sp&eacute;ciale</li>
+          <li>Mise en entrep&ocirc;t de stockage direct/Mise en entrep&ocirc;t suite admission temporaire normale</li>
+          <li>Transit-Transbordement</li>
+          <li>Exportation d&eacute;finitive/R&eacute;exportation suite admission temporaire</li>
+      
+      </ul>
     </article>
-    <div className="droite">NOS TRAVAUX</div>
+    <div className="droite">A PROPOS </div>
   </div>
   )
 }
